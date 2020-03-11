0e4f80534fc3f1e3fae869380fe4cddf8c75165b
min <robertmin522@gmail.com>
2020-03-11 14:59:00 +0900

    firestore user set

---
+++ lib/data/provider/my_user_data.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+import 'package:theham/data/user.dart';
+
+class MyUserData extends ChangeNotifier{
+  User _myUserData;
+
+  User get data => _myUserData;
+
+  void setUserData(User user){
+    _myUserData = user;
+    notifyListeners();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,28 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:theham/constants/material_white_color.dart';
+import 'package:theham/data/provider/my_user_data.dart';
 import 'package:theham/main_page.dart';
 import 'package:theham/screens/auth_page.dart';
 
-void main(){
-  return runApp(Theham());
+void main() {
+  return runApp(ChangeNotifierProvider<MyUserData>(
+      create: (context) => MyUserData(), child: Theham()));
 }
 
-class Theham extends StatelessWidget{
-
+class Theham extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: StreamBuilder<FirebaseUser>(
-        stream: FirebaseAuth.instance.onAuthStateChanged,
-        builder: (context, snapshot) {
-          if(snapshot.hasData) {
-            return MainPage();
-          }
-          return MainPage(); //AuthPage original
-        }
-      ),
-      theme: ThemeData(
-        primarySwatch: white
-      ),
+          stream: FirebaseAuth.instance.onAuthStateChanged,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return MainPage();
+            }
+            return MainPage(); //AuthPage original
+          }),
+      theme: ThemeData(primarySwatch: white),
     );
   }
 }
-
